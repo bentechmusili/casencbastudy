c1ef9c5b8603bff87625f74555a0edb6d06f5a13
Benjamin <bentechmusili@gmail.com>
2026-06-10 23:14:31 +0300

    code optimiazation

--- src/main/java/com/example/demo/apis/CountryController.java
+++ src/main/java/com/example/demo/apis/CountryController.java
@@ -1,5 +1,8 @@
 package com.example.demo.apis;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 
 import com.example.demo.models.CountryInfo;
 import com.example.demo.models.dto.CountryRequest;
@@ -8,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/countries")
 @RequiredArgsConstructor
@@ -17,8 +20,9 @@ public class CountryController {
     private final CountryService countryService;
 
 
-    @PostMapping("/create")
+    @PostMapping("/saveCountryInfo")
     public CountryInfo create(@RequestBody CountryRequest request) {
+        log.info("POST /saveCountryInfo hit");
         return countryService.createCountry(request.getName());
     }
 
